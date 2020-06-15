d410832d4422a3cf5a54fcbe07729be855b63ed3
muriloignaccio <murilohsignacio@gmail.com>
2020-06-15 04:36:15 -0300

    Import router component to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// import Loading from './components/Loading';
+import Routes from './pages';
+
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes />
     </div>
   );
 }
