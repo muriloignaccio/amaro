778c455392158fd5a1166010c5f9cffbc2e9d4b9
muriloignaccio <murilohsignacio@gmail.com>
2020-06-18 01:32:54 -0300

    Importar todos os componentes e exportá-los

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Background from './Background';
+import Bag from './Bag';
+import Button from './Button';
+import Header from './Header';
+import Loading from './Loading';
+import Menu from './Menu';
+import Product from './Product';
+
+export { Background, Bag, Button, Header, Loading, Menu, Product };
