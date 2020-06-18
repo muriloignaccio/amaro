fec831c14c47dfca8a1557d465d8c1e2fe8a36de
muriloignaccio <murilohsignacio@gmail.com>
2020-06-18 01:29:51 -0300

    Colocar provider no componente raiz

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 // import Loading from './components/Loading';
 import Routes from './pages';
+import store from './store';
 
 import './styles.scss';
 
 function App() {
   return (
     <div className='App'>
-      <Routes />
+      <Provider store={store}>
+        <Routes />
+      </Provider>
     </div>
   );
 }
