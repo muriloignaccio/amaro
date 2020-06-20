0f978afd790515942e9423cb055d80675d06fb06
muriloignaccio <murilohsignacio@gmail.com>
2020-06-20 03:26:06 -0300

    Create new route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Header from '../components/Header';
+import { Header, Products } from '../components';
 import Home from './Home';
 
 import './reset.scss';
@@ -12,6 +12,7 @@ export default () => {
       <Header />
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/collections/:type/:product?' component={Products} />
       </Switch>
     </BrowserRouter>
   );
